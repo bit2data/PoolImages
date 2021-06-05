1e9e6a79aeeef85f712bc5a124d4fd90fe7f430b
JJ Choix <>
2021-06-05 21:41:01 +0000

    incorrect width and height size in 000.json fixed

--- adhoc.py
+++ adhoc.py
@@ -14,7 +14,36 @@ def find_small(src):
       small[str(width)].append(pth)
   return small
 
+def run_find_small():
+  for src in ['test', 'train']:
+    print(find_small(src)['300'])
+  return 'done'
+  
+# [dir] Bool -> Int
+def correct_img_size(src, for_real=False):
+  where = './static/pools/images/{}/*.jpg'.format(src)
+  #print(where)
+  count = 0
+  for pth in glob.glob(where):
+    #print(pth)
+    img = Image.open(pth)
+    width, height = img.size
+    need_to_correct = True
+    json_path = pth.replace('jpg', 'json')
+    with open(json_path, 'r+') as f:
+      data = json.load(f)
+      if width == data['w'] and height == data['h']:
+        need_to_correct = False
+      else:
+        print('need to correct', data)
+      if for_real and need_to_correct:
+        data['w'] = width
+        data['h'] = height
+        f.seek(0)
+        json.dump(data, f)
+        count += 1
+  return count 
 
-for src in ['test', 'train']:
-  print(find_small(src)['300'])
-
+def run_correct_img_size():
+  for x in ['test', 'train']:
+    print(correct_img_size(x, for_real=True), 'files corrected')
